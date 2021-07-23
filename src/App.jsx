df0f1d8a65905a5db3a27d556cc09467f5a218d4
[suyash patel] <suyashpatelmrj@gmail.com>
2021-07-23 21:32:26 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ function App() {
   const items = [];
   for (let i = 1; i <= 100; i++) {
     let style = {};
-    if(selectedIndex !== null && i % selectedIndex === 0){
+     if(selectedIndex !== null && i % selectedIndex === 0){
         style = {
             backgroundColor: "yellow"
         };
     }
-    items.push(<div onClick={() => changeColor(i)} className="border border-gray-600 text-center" style={style}>
+   setInterval(items.push(<div onClick={() => changeColor(i)} className="border border-gray-600 text-center" style={style}>
       {i}
-    </div>)
+    </div>),200)
   }
   function changeColor(index) {
     // only need to set the state here
